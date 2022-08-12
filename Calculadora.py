606d1d388a4796114cab42dc717d2653579fc524
Juan <practicante.sistemas@camejia.com.co>
2022-08-12 09:46:37 -0500

    Cambios 2

--- Calculadora.py
+++ Calculadora.py
@@ -28,31 +28,31 @@ resultado = StringVar()
 
 entrada1 = Entry(frame)
 entrada1.pack()
-entrada1.config(bd=10, font=("Curier, 10"), textvariable=var1)
+entrada1.config(bd=10, bg="Aquamarine", font=("Curier, 10"), textvariable=var1)
 
 entrada2 = Entry(frame)
 entrada2.pack()
-entrada2.config(bd=10, font=("Curier, 10"), textvariable=var2)
+entrada2.config(bd=10, bg="Aquamarine", font=("Curier, 10"), textvariable=var2)
 
 entrada3 = Entry(frame)
 entrada3.pack()
-entrada3.config(bd=10, font=("Curier, 10"), textvariable=resultado, state="disable")
+entrada3.config(bd=10,  font=("Curier, 10"), textvariable=resultado, state="disable")
 
 boton1 = Button(frame, text="Sumar")
 boton1.pack()
-boton1.config(bd=5, font=("Curier, 10"), command=sumar)
+boton1.config(bd=5, bg="red", font=("Curier, 10"), command=sumar)
 
 boton2 = Button(frame, text="Restar")
 boton2.pack()
-boton2.config(bd=5, font=("Curier, 10"), command=restar)
+boton2.config(bd=5, bg="red", font=("Curier, 10"), command=restar)
 
 boton3 = Button(frame, text="Dividir")
 boton3.pack()
-boton3.config(bd=5, font=("Curier, 10"), command=dividir)
+boton3.config(bd=5, bg="red", font=("Curier, 10"), command=dividir)
 
 boton4 = Button(frame, text="Multiplicar")
 boton4.pack()
-boton4.config(bd=5, font=("Curier, 10"), command=multiplicar)
+boton4.config(bd=5, bg="red", font=("Curier, 10"), command=multiplicar)
 
 
 
