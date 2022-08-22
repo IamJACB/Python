fc19e509a93a84601ac99ea4c645661c3bd50480
Juan <practicante.sistemas@camejia.com.co>
2022-08-22 16:09:03 -0500

    spinbox

--- spinbox.py
+++ spinbox.py
@@ -3,10 +3,15 @@ from tkinter import messagebox
 
 
 root = Tk()
-root.geometry("400x400")
+root.geometry("400x300")
 
 w = Spinbox(root, values=("Python", "Html5", "Java", "Javascript"))
 w.pack()
+
+
+e = Spinbox(root, values=("Nodejs", "Html5", "Java", "Javascript"))
+e.pack()
+
 
 
 root.mainloop()
