d7f9826f152355c69159919ac046ad893c251b64
Juan <practicante.sistemas@camejia.com.co>
2022-09-01 15:56:21 -0500

    Metallica

--- barcode.py
+++ barcode.py
@@ -3,14 +3,11 @@ import png
 import PIL
 from PIL import Image
 
-
-
-
 link = input("Ingrese cualquier cosa:")
 qr_code = pyqrcode.create(link)
 qr_code.png("Qrcode.png", scale=5)
 
-PIL.Image.open(r"C:\Users\PRACTICANTE.SISTEMAS\Documents\Juan\Python\Curso Python\Interfaces graficas\Qrcode.png")
+Image.Open(r"C:\Users\PRACTICANTE.SISTEMAS\Documents\Juan\Python\Curso Python\Interfaces graficas\Qrcode.png")
 
 
  
